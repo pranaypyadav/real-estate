c7df1a08da668b4bb69746e6bce0d583d876b025
Pranay Yadav <pranayyadav7272@gmail.com>
2024-05-25 08:09:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      I am in the APP cmpnt
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            In Home
+
+        </div>
+    )
+}
+
+export default Home
