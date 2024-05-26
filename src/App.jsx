302403e1aedd912969a0b6bbd75fe85691fd4bd2
Pranay Yadav <pranayyadav7272@gmail.com>
2024-05-26 09:52:14 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from './Components/Home'
-import SignIn from './Components/SignIn'
-import Profile from './Components/Profile'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/profile/:id/:user' element={<Profile />}></Route>
-        <Route path='/signin' element={<SignIn />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      Hello
+    </div>
   )
 }
 
