98427021c705e625085ea99f018221518191ce17
Pranay Yadav <pranayyadav7272@gmail.com>
2024-05-27 19:48:50 +0530

    made some changes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,8 @@
 import express from "express"
-// import mongoose from "mongoose"
 
 
-
-// mongoose.connect("mongodb")
 const app = express()
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000 and we have added nodemon")
+    console.log("Server is running on port 3000")
 })
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
                     <input type="text" placeholder='Search...' className=' outline-none bg-transparent ' />
                     <FaSearch className=' text-slate-600'></FaSearch>
                 </form>
-                <ul className=' flex gap-4 items-center '>
+                <ul className=' flex gap-4 '>
                     <Link to={"/"}><li className=' hidden sm:inline text-slate-700 hover:underline '>Home</li></Link>
                     <Link to={'/about'}>  <li className=' hidden sm:inline text-slate-700 hover:underline'>About</li> </Link>
                     <Link to={"/sign-in"}> <li className='  sm:inline text-slate-700 hover:underline'>Sign In</li></Link>
