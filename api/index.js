c3d30727b1274d7053dba6fb57823cfbe7dfb3db
Pranay Yadav <pranayyadav7272@gmail.com>
2024-05-27 19:42:24 +0530

    edited indexjs

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from "express"
+// import mongoose from "mongoose"
 
 
+
+// mongoose.connect("mongodb")
 const app = express()
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3000 and we have added nodemon")
 })
